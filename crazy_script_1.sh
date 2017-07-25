4d20050f41cb500cdb876a1ef410a82399f1db93
Ebro007 <30446113+Ebro007@users.noreply.github.com>
2017-07-25 19:47:00 +0000

    uploaded four text files from pi
    Te llamo no bueno. Te llamo no bueno.

---
+++ crazy_script_1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This is a comment
+#list files
+ls
+
+#redirect the output of ls to "Malory"
+ls > Malory
+
+#Print a blank line
+echo " "
+
+#print "catting Malory
+echo "catting Malory"
+
+#Print the contents of the file "Malory"
+cat Malory
+
+#Print an integer
+echo "1234"
+
+#Print a word
+echo "Word"
+
+#print something
+echo "Location"
+
+#Print current location
+pwd
+
